8c57d75bdd5e872e2c54f9eeb53b2241507ae978
AlexMilhomes <alexmilhomes@gmail.com>
2020-06-17 20:52:21 -0300

    ProjetoApp

--- lib/view/cadastrar.dart
+++ lib/view/cadastrar.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 //void main() => runApp(new MyApp());
@@ -9,6 +8,8 @@ class Cadastrar extends StatefulWidget {
 }
 
 class _CadastrarState extends State<Cadastrar> {
+  List<String> dataList = [];
+
   GlobalKey<FormState> _key = new GlobalKey();
   bool _validate = false;
   String titulo, date, participantes;
@@ -19,7 +20,8 @@ class _CadastrarState extends State<Cadastrar> {
       debugShowCheckedModeBanner: false,
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Ata de Reunião'),
+          centerTitle: true,
+          title: new Text('Ata de Reunião '),
           backgroundColor: Colors.deepOrange,
         ),
         body: new SingleChildScrollView(
@@ -41,7 +43,9 @@ class _CadastrarState extends State<Cadastrar> {
     return new Column(
       children: <Widget>[
         new TextFormField(
-          decoration: new InputDecoration(hintText: 'Título'),
+          decoration: new InputDecoration(
+              labelText: "Título: ",
+              labelStyle: TextStyle(color: Colors.black)),
           maxLength: 40,
           validator: _validarTitulo,
           onSaved: (String val) {
@@ -49,7 +53,9 @@ class _CadastrarState extends State<Cadastrar> {
           },
         ),
         new TextFormField(
-            decoration: new InputDecoration(hintText: 'Data de Emissão'),
+            decoration: new InputDecoration(
+                labelText: "Data de Emissão:",
+                labelStyle: TextStyle(color: Colors.black)),
             keyboardType: TextInputType.datetime,
             maxLength: 8,
             validator: _validarDate,
@@ -59,35 +65,80 @@ class _CadastrarState extends State<Cadastrar> {
         new TextFormField(
             minLines: 1,
             maxLines: 100,
-            decoration: new InputDecoration(hintText: 'Participantes'),
+            decoration: InputDecoration(
+                labelText: "Participantes",
+                labelStyle: TextStyle(color: Colors.black)),
             maxLength: 40,
             validator: _validarParticipantes,
             onSaved: (String val) {
               participantes = val;
             }),
-      
-            
-        //SizedBox(
-          //width: 200,
-          //height: 100,
-           TextFormField(
-            //autofocus: true,
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              labelText: "Hora de Inicio da Reunião:",
-              labelStyle: TextStyle(
-                color: Colors.black,
-                fontSize: 15,
-              ),
-            ),
-            style: TextStyle(
-              fontSize: 28,
+
+        TextFormField(
+          //autofocus: true,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            labelText: "Hora de Inicio da Reunião:",
+            labelStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
             ),
           ),
-          //height: 20,
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+
+        TextFormField(
+          //autofocus: true,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            labelText: "Hora de Termino da Reunião:",
+            labelStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+            ),
+          ),
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+
+        TextFormField(
+          minLines: 1,
+          maxLines: 100,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            labelText: "Pauta da Reunião:",
+            labelStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+            ),
+          ),
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+
+        TextFormField(
+          //autofocus: true,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            labelText: "Informe o Setor:",
+            labelStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+            ),
+          ),
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+
+        //height: 20,
         //),
-        new SizedBox(height: 15.0),
-        new RaisedButton(
+        SizedBox(height: 15.0),
+        RaisedButton(
           onPressed: _sendForm,
           child: new Text('Registrar Ata'),
         ),
@@ -128,32 +179,6 @@ class _CadastrarState extends State<Cadastrar> {
     return null;
   }
 
-  // String _validarData(String value) {
-  //   String patttern = r'(^[0-9]*$)';
-  //   RegExp regExp = new RegExp(patttern);
-  //   if (value.length == 0) {
-  //     return "Informe a Data";
-  //   } else if(value.length != 7){
-  //     return "O celular deve ter 10 dígitos";
-  //   }else if (!regExp.hasMatch(value)) {
-  //     return "O número do celular so deve conter dígitos";
-  //   }
-  //   return null;
-  // }
-
-  // String _validarEmail(String value) {
-  //   String pattern =
-  //       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-  //   RegExp regExp = new RegExp(pattern);
-  //   if (value.length == 0) {
-  //     return "Informe o Email";
-  //   } else if (!regExp.hasMatch(value)) {
-  //     return "Email inválido";
-  //   } else {
-  //     return null;
-  //   }
-  // }
-
   _sendForm() {
     if (_key.currentState.validate()) {
       _key.currentState.save();
@@ -167,121 +192,4 @@ class _CadastrarState extends State<Cadastrar> {
     }
   }
 }
-// import 'package:flutter/material.dart';
-
-// class CadastrarAta extends StatelessWidget {
-//   const CadastrarAta({Key key}) : super(key: key);
-
-//   //@override
-//   //_ResultadoPageState creatState() => _ResultadoPageState();
-
-//   @override
-//   Widget build(BuildContext context) {
-//      return Scaffold(
-//       body: Container(
-//           padding: EdgeInsets.only(top: 10, left: 40, right: 0),
-//           color: Colors.white,
-//           child: ListView(
-//              children: <Widget>[
-//     // var listView = ListView.builder(
-//     //         itemCount: dataList.length,
-//     //         itemBuilder: (_, int index){
-//     //           return Text(dataList[index]);
-//     //         },
-//               SizedBox(
-//                 height: 20,
-//               ),
-//               TextFormField(
-//                 //autofocus: true,
-//                 keyboardType: TextInputType.text,
-//                 decoration: InputDecoration(
-//                   labelText: "Título:",
-//                   labelStyle: TextStyle(
-//                     color: Colors.black,
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//                 style: TextStyle(
-//                   fontSize: 28,
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 18,
-//               ),
-//               TextFormField(
-//                 //autofocus: true,
-//                 keyboardType: TextInputType.datetime,
-//                 decoration: InputDecoration(
-//                   labelText: "Data da Emissão:",
-//                   labelStyle: TextStyle(
-//                     color: Colors.black,
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//                 style: TextStyle(
-//                   fontSize: 28,
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 18,
-//               ),
-//               TextFormField(
-//                 //autofocus: true,
-//                 keyboardType: TextInputType.text,
-//                 decoration: InputDecoration(
-//                   labelText: "Participantes:",
-//                   labelStyle: TextStyle(
-//                     color: Colors.black,
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//                 style: TextStyle(
-//                   fontSize: 28,
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 18,
-//               ),
-//               TextFormField(
-//                 //autofocus: true,
-//                 keyboardType: TextInputType.datetime,
-//                 decoration: InputDecoration(
-//                   labelText: "Hora Inicio:",
-//                   labelStyle: TextStyle(
-//                     color: Colors.black,
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//                 style: TextStyle(
-//                   fontSize: 28,
-//                 ),
-//               ),
-//               SizedBox(
-//                 height: 18,
-//               ),
-
-//                   RaisedButton(
-//   onPressed: () {
-//     var _formKey;
-//         if (_formKey.currentState.validate()) {
-//       Scaffold
-//           .of(context)
-//           .showSnackBar(SnackBar(content: Text('Dados processados com sucesso')));
-//     }
-//   },
-//   child: Text('Registrar Ata'),
-// )
-//             //    RaisedButton(
-//             //   child: Text("Registrar"),
-//             //   onPressed: (){
-//             //     var controller;
-//             //     Navigator.of(context).pop(controller.text);
-//             //   },
-//             // ),
-
-//             ]
-//           )));
-
-//   }
-
-// }
+//
